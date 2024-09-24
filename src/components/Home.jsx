94456013f201d62d574e21b80149edca7dbc58ef
Melany0802 <melcris0802@estudiantec.cr>
2024-09-24 15:18:13 -0600

    first commit - header

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../components/home.css'
+
+function Home(){
+    return(
+        <section>
+            <header className='NavigationBar'>
+                <h1 className='title'>My To-Do </h1>
+                <button className='addButton'>+ Add Task</button>
+            </header>
+            <section>
+
+            </section>
+        </section>
+    )
+}
+
+export default Home;
